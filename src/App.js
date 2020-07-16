42ac4f365573c546e6004ad781f664b1b16b1dd2
josejpm <josejpm@gmail.com>
2020-07-15 23:18:14 -0300

    Recent added done

--- src/App.js
+++ src/App.js
@@ -6,18 +6,23 @@ import Footer from './components/footer/Footer';
 import AboutUs from './components/about/About-Us';
 import Header from './components/header/Header'
 import Properties from './components/properties/Properties';
+import Recent from './components/recent/Recent';
+
+
 
 
 
 
 function App() {
   return (
-  <div className="site-container">
-    <Header/>   
-    <AboutUs/>
-    <Properties/>
-    <Footer/>
-  </div>
+      <div className="site-container">
+        <Header/>   
+        <AboutUs/>
+        <Properties/>
+        <Recent/>
+        <Footer/>
+      </div>
+
   )
 }
 
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React from "react";
 import BlueButton from '../utils/blueButton/BlueButton'
 import "./Header.scss";
 
